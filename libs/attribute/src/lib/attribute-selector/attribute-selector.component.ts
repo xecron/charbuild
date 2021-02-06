e5c0dd391f080684dfb779213013a9f96eaae8aa
xecron <batuhancimen.x@gmail.com>
2021-02-06 20:10:36 +0100

    feat: add attribute selector ui component
    Signed-off-by: xecron <batuhancimen.x@gmail.com>

---
+++ libs/attribute/src/lib/attribute-selector/attribute-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'charbuild-attribute-selector',
+  templateUrl: './attribute-selector.component.html',
+  styleUrls: ['./attribute-selector.component.scss']
+})
+export class AttributeSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/attribute/src/lib/attribute.module.ts
+++ libs/attribute/src/lib/attribute.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AttributeService } from './attribute.service';
+import { AttributeSelectorComponent } from './attribute-selector/attribute-selector.component';
 
 @NgModule({
   imports: [CommonModule],
   providers: [AttributeService],
+  declarations: [AttributeSelectorComponent],
+  exports: [AttributeSelectorComponent],
 })
 export class AttributeModule {}
