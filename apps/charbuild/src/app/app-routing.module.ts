ee53ac2cf7e208bb228f3e1bf5d12295337b8e9a
xecron <batuhancimen.x@gmail.com>
2021-02-27 18:01:33 +0100

    refactor: move shared module to library
    Signed-off-by: xecron <batuhancimen.x@gmail.com>

--- apps/charbuild/src/app/app-routing.module.ts
+++ apps/charbuild/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 
 export const routes = [
     {
--- apps/charbuild/src/app/app.module.ts
+++ apps/charbuild/src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { SharedModule } from './shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AttributeModule } from '@charbuild/attribute';
+import { SharedModule } from '@charbuild/shared';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, AttributeModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
